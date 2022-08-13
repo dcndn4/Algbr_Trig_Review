10529c7b3d602e14e43944f1468aee29881befd5
dcndn4 <stokes@iphouse.com>
2022-08-13 08:56:53 -0500

    math is best

--- Chptr_one.py
+++ Chptr_one.py
@@ -9,6 +9,8 @@ Created on Sat Aug 13 07:50:47 2022
 
 #To start with sets
 
+import math
+
 whole={1, 2, 3, 4}
 other={6,7,8}
 #%%
@@ -27,3 +29,70 @@ whole={15, 18, 21}
 set=set-{6}
 
 #%%
+
+x=5*6
+#%%
+
+y=5+10
+#%%
+
+z=2+5*8
+#%%
+
+a=(5+3)*4
+#%%
+
+b=(4+5)*(8-2)
+#%%
+c=2+(4+2*(10+6))
+#%%
+d=6-8
+#%%
+e=8/4
+#%%
+f=5/8
+#%%
+
+g=8/15+5/12
+#%%
+
+b=3>7
+#%%
+
+h=abs(-7)
+#%%
+
+x=3
+y=-1
+i=x+3*y
+
+#%%
+j=5*x*y
+k=3*y/2-2*x
+l=3*y/(2-2*x)
+#%%
+
+m=abs(-4*x+y)
+#%%
+# exponent .. I think we did it a different way in class ?!
+n=pow(3,4)
+#%%
+
+# that's right, double-multiply kind of..
+
+o=3**4
+#%%
+
+to get value of pi
+
+print(math.pi)
+#%%
+circumference
+radius = 15
+
+circ = (math.pi)*2*15
+#%%
+
+
+
+
