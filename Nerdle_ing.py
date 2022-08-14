5a7f3d92ba306efc4b3d80f42ea84f7321247429
dcndn4 <stokes@iphouse.com>
2022-08-14 06:14:11 -0500

    win in 4!

---
+++ Nerdle_ing.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 14 05:57:25 2022
+
+@author: CS_Knit_tinK_SC
+"""
+
+# Nerdle Adventures
+
+# Python as graphing calculator
+
+#  _ _ () _ _ = _ _ 
+# no 9, 8, 5, 3, 6
+# somewhere is 7, 2, 1, 4 
+# don't know about   0
+# don't use -
+
+# _ _ + _ _ = 17, 14, 16, 47, 42, 41, 46, 70, 71, 74
+# 21+46=57
+
+# one of these times I'll start with loops to figure this all out, just doing dry run now
+
+# _ _ + _ _ = 
+
+i=64-17
+i=41-17
+
+# not-2 | 4 + 27 = not-4|1
+
+i=27+44 
+
+#%%
+
+#44+27=71  win, guess # 4!
+#%%
